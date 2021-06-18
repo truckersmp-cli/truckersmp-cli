7adfb39bbd70010f5fdeb5a1b71f6c79d7d37e3f
Bimo Kristianto N <bimokn97@gmail.com>
2021-06-18 18:11:55 +0700

    Update utils.py
    adding proton-tkg support mayor-minor

--- truckersmp_cli/utils.py
+++ truckersmp_cli/utils.py
@@ -326,13 +326,18 @@ def get_proton_version(protondir):
     Examples:
       "xxxxxxxxxx proton-5.13-6" -> (5, 13)
       "xxxxxxxxxx 6.1-GE-2" -> (6, 1)
+      "xxxxxxxxxx proton-tkg-6.8.r15.gf308782d -> (6,8)"
 
     protondir: Proton top directory that has "version" file
     """
     with open(os.path.join(protondir, "version")) as f_version:
         ver = f_version.read(128)
-    ver = ver.replace("proton-", "")
-    major, minor = ver[ver.index(" ") + 1:ver.index("-")].split(".")
+    if ver.find("tkg") > 0:
+        ver = ver.replace("proton-tkg-", "")
+        major, minor = ver[ver.index(" ") + 1:ver.index(".r")].split(".")
+    else :
+        ver = ver.replace("proton-", "")
+        major, minor = ver[ver.index(" ") + 1:ver.index("-")].split(".")
     return int(major), int(minor)
 
 
